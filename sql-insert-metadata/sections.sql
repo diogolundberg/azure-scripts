1dcc0627fc894c4476dd1afdf389ebfa9b687939
Diogo Lundberg <dclundberg@gmail.com>
2018-04-03 17:36:23 -0300

    Add script for sections insertion

---
+++ sql-insert-metadata/sections.sql
@@ -0,0 +1,13 @@
+INSERT INTO [dbo].[Sections] ([CreatedAt], [Name]) VALUES (GETDATE(), 'Dados Pessoais');
+
+INSERT INTO [dbo].[Sections] ([CreatedAt], [Name]) VALUES (GETDATE(), 'Dados de Contato');
+
+INSERT INTO [dbo].[Sections] ([CreatedAt], [Name]) VALUES (GETDATE(), 'Endereço');
+
+INSERT INTO [dbo].[Sections] ([CreatedAt], [Name]) VALUES (GETDATE(), 'Dados para o Censo');
+
+INSERT INTO [dbo].[Sections] ([CreatedAt], [Name]) VALUES (GETDATE(), 'Outras Informações');
+
+INSERT INTO [dbo].[Sections] ([CreatedAt], [Name]) VALUES (GETDATE(), 'Necessidades Especiais');
+
+INSERT INTO [dbo].[Sections] ([CreatedAt], [Name]) VALUES (GETDATE(), 'Documentos');
