c27a9656d48770a59d01df90527c82624dba226e
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-04 11:30:18 -0300

    Change scripts

--- sql-insert-metadata/genders.sql
+++ sql-insert-metadata/genders.sql
@@ -1,4 +1,4 @@
-INSERT INTO Genders (CreatedAt, Name) VALUES
-(getdate(), 'Masculino'),
-(getdate(), 'Feminino'),
-(getdate(), 'Outros');
+INSERT INTO Genders (CreatedAt, Name,CheckMilitaryDraft) VALUES
+(getdate(), 'Masculino', 1),
+(getdate(), 'Feminino',0),
+(getdate(), 'Outros',0);
