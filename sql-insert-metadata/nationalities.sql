d6332f3cdf2dc38b96a29d956a8fc1d780f15fd0
Lucas Costa <lucasviniciusbatistacosta@gmail.com>
2018-04-08 16:32:45 -0300

    Fix nationalities sql.

--- sql-insert-metadata/nationalities.sql
+++ sql-insert-metadata/nationalities.sql
@@ -1,4 +1,4 @@
-INSERT INTO Nationalities (CreatedAt, Name, CheckForeign) VALUES
-(getdate(), 'Brasileira', 0),
-(getdate(), 'Brasileira – nascido no exterior ou naturalizado', 0),
-(getdate(), 'Estrangeira', 1);
+INSERT INTO Nationalities (CreatedAt, Name, CheckForeign, CheckNative) VALUES
+(getdate(), 'Brasileira', 0, 1),
+(getdate(), 'Brasileira – nascido no exterior ou naturalizado', 0, 1),
+(getdate(), 'Estrangeira', 1, 0);
