de803df4740ea192d01abc48790773f7446ae137
LUCAS VINÍCIUS BATISTA COSTA <lucasvbc@live.com>
2018-05-28 13:34:38 -0300

    Add script for steps.

---
+++ onboarding/steps.sql
@@ -0,0 +1,6 @@
+insert into steps(createdat, [name],[order],[resource]) values (getdate(), 'Dados pessoais', 10, 'PersonalDatas')
+insert into steps(createdat, [name],[order],[resource]) values (getdate(), 'Informações da Matrícula', 20, 'CourseSummaries')
+insert into steps(createdat, [name],[order],[resource]) values (getdate(), 'Dados financeiros', 30, 'FinanceDatas')
+insert into steps(createdat, [name],[order],[resource]) values (getdate(), 'Enviar para aprovação', 40, 'EnrollmentSummaries')
+insert into steps(createdat, [name],[order],[resource]) values (getdate(), 'Contrato', 50, 'Contracts')
+insert into steps(createdat, [name],[order],[resource]) values (getdate(), 'Pagamento', 60, 'Payments')
